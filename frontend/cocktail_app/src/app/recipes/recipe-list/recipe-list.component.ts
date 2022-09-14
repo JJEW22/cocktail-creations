cf4d28c23f5e759dd3989cf8c242c4b7b500aca7
John Wilkins <john.e.wilkins22@gmail.com>
2022-09-14 15:46:19 -0400

    added basic cocktail recipe list

--- frontend/cocktail_app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/cocktail_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('test recipe 1', 'TR1 description'),
+    new Recipe('test recipe 2', 'TR2 description'),
+  ];
   constructor() { }
 
   ngOnInit(): void {
---
+++ frontend/cocktail_app/src/app/recipes/recipe.model.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    // TODO: add ingredients
+    
+    constructor(name:string, description: string) {
+        this.name = name;
+        this.description = description;
+    }
+}
